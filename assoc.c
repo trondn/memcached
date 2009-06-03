a519192ec36eb66b4c69342d454d684e5b031ebd
Trond Norbye <Trond.Norbye@sun.com>
2009-06-03 12:42:28 +0200

    Remove obsolete comment in file header

--- assoc.c
+++ assoc.c
@@ -1,15 +1,4 @@
 /* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/*
- * Hash table
- *
- * The hash function used here is by Bob Jenkins, 1996:
- *    <http://burtleburtle.net/bob/hash/doobs.html>
- *       "By Bob Jenkins, 1996.  bob_jenkins@burtleburtle.net.
- *       You may use this code any way you wish, private, educational,
- *       or commercial.  It's free."
- *
- * The rest of the file is licensed under the BSD license.  See LICENSE.
- */
 
 #include "memcached.h"
 #include <sys/stat.h>
